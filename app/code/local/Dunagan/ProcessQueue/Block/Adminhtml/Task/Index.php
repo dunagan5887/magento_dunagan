857875c4c5b6778f868639d11fd96ce06bf21f6d
Sean Dunagan <dunagan5887@gmail.com>
2015-12-10 03:02:08 -0600

    Adding js confirmation to clear task buttons

--- app/code/local/Dunagan/ProcessQueue/Block/Adminhtml/Task/Index.php
+++ app/code/local/Dunagan/ProcessQueue/Block/Adminhtml/Task/Index.php
@@ -7,6 +7,8 @@
 class Dunagan_ProcessQueue_Block_Adminhtml_Task_Index
     extends Dunagan_Base_Block_Adminhtml_Widget_Grid_Container
 {
+    const BUTTON_ACTION_TEMPLATE = "confirmSetLocation('%s', '%s')";
+
     protected $_show_clear_tasks_buttons = true;
 
     /**
@@ -24,44 +26,66 @@ class Dunagan_ProcessQueue_Block_Adminhtml_Task_Index
         return $this->getAction()->getFullBackControllerActionPath();
     }
 
-    public function getActionButtonsToRender()
+    public function __construct()
     {
-        $buttons_to_render = parent::getActionButtonsToRender();
+        parent::__construct();
 
-        if ($this->_show_clear_tasks_buttons)
-        {
-            $task_codes = $this->getTaskJobCodes();
-            if (is_array($task_codes) && (!empty($task_codes)))
-            {
-                $imploded_task_codes = implode(';', $task_codes);
-                $task_code_param = urlencode($imploded_task_codes);
-            }
-            else
-            {
-                $task_code_param = null;
-            }
-            $encoded_redirect_route = urlencode($this->getRedirectRoute());
-
-            $clear_all_tasks_button = array(
-                'action_url' => Mage::getModel('adminhtml/url')->getUrl('adminhtml/DunaganProcessQueue_index/clearAllTasks',
-                    array('task_codes' => $task_code_param,
-                        'redirect_route' => $encoded_redirect_route)
-                ),
-                'label' => 'Clear All Tasks'
-            );
-
-            $clear_successful_tasks_button = array(
-                'action_url' => Mage::getModel('adminhtml/url')->getUrl('adminhtml/DunaganProcessQueue_index/clearSuccessfulTasks',
-                    array('task_codes' => $task_code_param,
-                        'redirect_route' => $encoded_redirect_route)
-                ),
-                'label' => 'Clear Successful Sync Tasks'
-            );
-
-            $buttons_to_render['clear_all_sync_tasks'] = $clear_all_tasks_button;
-            $buttons_to_render['clear_successful_sync_tasks'] = $clear_successful_tasks_button;
+        if ($this->_show_clear_tasks_buttons) {
+            $this->_addClearTasksButtons();
         }
+    }
 
-        return $buttons_to_render;
+    public function _addClearTasksButtons()
+    {
+        $buttons_to_render = array();
+
+        $task_codes = $this->getTaskJobCodes();
+        if (is_array($task_codes) && (!empty($task_codes)))
+        {
+            $imploded_task_codes = implode(';', $task_codes);
+            $task_code_param = urlencode($imploded_task_codes);
+        }
+        else
+        {
+            $task_code_param = null;
+        }
+        $encoded_redirect_route = urlencode($this->getRedirectRoute());
+
+        $clear_all_tasks_button = array(
+            'action_url' => Mage::getModel('adminhtml/url')->getUrl('adminhtml/DunaganProcessQueue_index/clearAllTasks',
+                array('task_codes' => $task_code_param,
+                    'redirect_route' => $encoded_redirect_route)
+            ),
+            'label' => 'Clear All Tasks',
+            'confirm_message' => 'Are you sure you want to clear all tasks?'
+        );
+
+        $clear_successful_tasks_button = array(
+            'action_url' => Mage::getModel('adminhtml/url')->getUrl('adminhtml/DunaganProcessQueue_index/clearSuccessfulTasks',
+                array('task_codes' => $task_code_param,
+                    'redirect_route' => $encoded_redirect_route)
+            ),
+            'label' => 'Clear Successful Sync Tasks',
+            'confirm_message' => 'Are you sure you want to clear all successful tasks?'
+        );
+
+        $buttons_to_render['clear_all_sync_tasks'] = $clear_all_tasks_button;
+        $buttons_to_render['clear_successful_sync_tasks'] = $clear_successful_tasks_button;
+
+        foreach ($buttons_to_render as $button_id => $button)
+        {
+            $label = $this->getAction()->getModuleHelper()->__($button['label']);
+            $confirm_message = $this->getAction()->getModuleHelper()->__($button['confirm_message']);
+            $action_url = $button['action_url'];
+            $onclick = sprintf(self::BUTTON_ACTION_TEMPLATE, $confirm_message, $action_url);
+
+            $this->_addButton(
+                $button_id, array(
+                    'label' => $this->getAction()->getModuleHelper()->__($label),
+                    'onclick' => $onclick,
+                    'level' => -1
+                )
+            );
+        }
     }
 }
