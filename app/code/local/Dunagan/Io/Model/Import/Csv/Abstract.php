060dfe1a43badd93c96a5b30697ab6755e9ada5e
Sean Dunagan <dunagan5887@gmail.com>
2015-12-17 20:38:57 -0600

    Updating the CSV abstract class to allow for defining a different array to be used as the keys for the  data array

--- app/code/local/Dunagan/Io/Model/Import/Csv/Abstract.php
+++ app/code/local/Dunagan/Io/Model/Import/Csv/Abstract.php
@@ -23,6 +23,7 @@ abstract class Dunagan_Io_Model_Import_Csv_Abstract
     protected $_suppress_row_validation_errors = false;
 
     protected $_expected_row_size = null;
+    protected $_array_to_use_for_data_array_preparation = null;
 
     public function readFile($ioAdapter, $file, $file_path)
     {
@@ -30,11 +31,11 @@ abstract class Dunagan_Io_Model_Import_Csv_Abstract
         $this->_row_number = 0;
 
         // Build array, cells with header names.
-        $headerRow = $this->_getHeaderRow($ioAdapter);
+        $header_row = $this->_getHeaderRow($ioAdapter);
 
         $this->_header_row = null;
 
-        if (!$this->validateHeader($headerRow, $file['text']))
+        if (!$this->validateHeader($header_row, $file['text']))
         {
             // Reset this->_header_row in case a subclass has set it
             // Method _validateHeader() is expected to have logged any necessary error messaging
@@ -42,7 +43,7 @@ abstract class Dunagan_Io_Model_Import_Csv_Abstract
             return false;
         }
 
-        $this->_header_row = $headerRow;
+        $this->_header_row = $header_row;
 
         while(false !== ($row = $this->_getNextRow($ioAdapter)))
         {
@@ -57,7 +58,7 @@ abstract class Dunagan_Io_Model_Import_Csv_Abstract
                 continue;
             }
 
-            $toProcess = $this->_prepareForProcessing($headerRow, $row);
+            $toProcess = $this->_prepareForProcessing($header_row, $row);
             $this->_processRow($this->_row_number, $toProcess, $file['text']);
             $row = false;
         }
@@ -100,14 +101,14 @@ abstract class Dunagan_Io_Model_Import_Csv_Abstract
         $this->importDataRow($rowData, $row_num);
     }
 
-    public function validateHeader(array $headerRow, $fileName)
+    public function validateHeader(array $header_row, $fileName)
     {
         $is_header_valid = true;
 
-        if (!$this->_validateRowSize($headerRow))
+        if (!$this->_validateRowSize($header_row))
         {
             $expected_row_size = $this->_getExpectedRowSize();
-            $actual_row_size = count($headerRow);
+            $actual_row_size = count($header_row);
             $this->logError(
                 sprintf(self::FILE_HEADER_INVALID_SIZE, $fileName, $expected_row_size, $actual_row_size)
             );
@@ -119,7 +120,7 @@ abstract class Dunagan_Io_Model_Import_Csv_Abstract
 
             foreach($required_headers as $required_header)
             {
-                $array_search_value = array_search($required_header, $headerRow);
+                $array_search_value = array_search($required_header, $header_row);
                 if($array_search_value === FALSE)
                 {
                     $this->logError(
@@ -132,7 +133,7 @@ abstract class Dunagan_Io_Model_Import_Csv_Abstract
 
         if (!$is_header_valid)
         {
-            $this->actOnInvalidHeaderFile($fileName, $headerRow);
+            $this->actOnInvalidHeaderFile($fileName, $header_row);
         }
 
         return $is_header_valid;
@@ -167,9 +168,24 @@ abstract class Dunagan_Io_Model_Import_Csv_Abstract
         return $this->_expected_row_size;
     }
 
-    protected function _prepareForProcessing($headerRow, $row)
+    protected function _prepareForProcessing($header_row, $row)
     {
-        return new Varien_Object(array_combine($headerRow, $row));
+        return new Varien_Object(array_combine($this->_getArrayToUseForDataPreparation($header_row), $row));
+    }
+
+    protected function _getArrayToUseForDataPreparation($header_row)
+    {
+        if (is_null($this->_array_to_use_for_data_array_preparation))
+        {
+            $this->_array_to_use_for_data_array_preparation = $this->getRowDataObjectKeysArray($header_row);
+        }
+
+        return $this->_array_to_use_for_data_array_preparation;
+    }
+
+    public function getRowDataObjectKeysArray($header_row)
+    {
+        return $header_row;
     }
 
     public function resetForNextReadPhase()
@@ -183,7 +199,7 @@ abstract class Dunagan_Io_Model_Import_Csv_Abstract
     /*
      * The following methods may be overridden by subclasses if necessary
      */
-    public function actOnInvalidHeaderFile($fileName, $headerRow)
+    public function actOnInvalidHeaderFile($fileName, $header_row)
     {
         return true;
     }
