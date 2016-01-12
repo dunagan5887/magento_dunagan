cce567e80bfa5ee7c4f82ee00c75f8e0bbf0c64a
Sean Dunagan <dunagan5887@gmail.com>
2016-01-12 14:35:06 -0600

    Updating Admin grid datetime column renderer

--- app/code/local/Dunagan/Base/Block/Adminhtml/Widget/Grid/Column/Renderer/Datetime.php
+++ app/code/local/Dunagan/Base/Block/Adminhtml/Widget/Grid/Column/Renderer/Datetime.php
@@ -7,14 +7,29 @@
 class Dunagan_Base_Block_Adminhtml_Widget_Grid_Column_Renderer_Datetime
     extends Mage_Adminhtml_Block_Widget_Grid_Column_Renderer_Datetime
 {
+    CONST DEFAULT_DATETIME_FORMAT = 'Y-m-d g:i:s A';
+
     public function render(Varien_Object $row)
     {
         $data = $this->_getValue($row);
-        if (!strcmp('0000-00-00 00:00:00', $data))
-        {
+        if (!strcmp('0000-00-00 00:00:00', $data)) {
             return '';
         }
 
-        return parent::render($row);
+        if (!$this->getColumn()->getDoNotConvertDatetime())
+        {
+            return parent::render($row);
+        }
+
+        $format = $this->getColumn()->getFormat();
+        if (empty($format))
+        {
+            $format = self::DEFAULT_DATETIME_FORMAT;
+        }
+
+        $timestamp = strtotime($data);
+        $data = date($format, $timestamp);
+
+        return $data;
     }
 }
